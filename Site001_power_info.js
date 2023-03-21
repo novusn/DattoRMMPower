8f1090ac698bd8389819a0c5d39711d113b02409
Mark Birchall <mark.birchall@novus.co.uk>
2023-03-21 17:54:37 +0000

    Colours

--- Site001_power_info.js
+++ Site001_power_info.js
@@ -6,9 +6,20 @@ function displayPowerInfo(data) {
     data.forEach(row => {
         const tr = document.createElement('tr');
         
-        row.forEach(cell => {
+        row.forEach((cell, index) => {
             const td = document.createElement('td');
             td.textContent = cell;
+            
+            // Check if the current cell is the Status cell (index 3)
+            if (index === 3) {
+                // Apply green color if the status is Active, red if Inactive
+                if (cell.trim().toLowerCase() === 'active') {
+                    td.style.color = 'green';
+                } else if (cell.trim().toLowerCase() === 'inactive') {
+                    td.style.color = 'red';
+                }
+            }
+
             tr.appendChild(td);
         });
 
