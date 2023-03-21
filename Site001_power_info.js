d2a71331f6787b8936815b7d726af25d70fd9f61
Mark Birchall <mark.birchall@novus.co.uk>
2023-03-21 17:14:47 +0000

    Add files via upload

---
+++ Site001_power_info.js
@@ -0,0 +1,28 @@
+const csvUrl = 'https://github.com/novusn/DattoRMMPower/blob/main/Site001.csv';
+
+function displayPowerInfo(data) {
+    const tableBody = document.getElementById('powerInfoTableBody');
+    
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+        
+        row.forEach(cell => {
+            const td = document.createElement('td');
+            td.textContent = cell;
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+}
+
+Papa.parse(csvUrl, {
+    download: true,
+    header: false,
+    complete: (results) => {
+        displayPowerInfo(results.data);
+    },
+    error: (error) => {
+        console.error('Error while parsing the CSV file:', error);
+    }
+});
